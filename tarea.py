5359e9bd32bd85ac4865e10de973fd88b457ad9d
Ajgr <alimigarmendia@gmail.com>
2020-02-29 23:49:24 -0400

    Se agrego una verificacion de status a la solicitud.

--- tarea.py
+++ tarea.py
@@ -2,5 +2,6 @@ import requests
 from pprint import pprint
 
 response = requests.post('https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token', auth=('devnetuser','Cisco123!'))
+response.raise_for_status()
 payload=response.json()
 pprint(payload)
